22cbe0aba7037e114da7c4a3a0701ea1d79d3e40
noirecat <myugan59@gmail.com>
2018-02-28 22:21:29 +0700

    updated

---
+++ .local/share/nautilus-python/extensions/Backspace-Back.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# by Ricardo Lenz, 2016-jun
+# riclc@hotmail.com
+#
+
+import os, gi
+gi.require_version('Nautilus', '3.0')
+from gi.repository import GObject, Nautilus, Gtk, Gio, GLib
+
+def ok():
+    app = Gtk.Application.get_default()
+    app.set_accels_for_action( "win.up", ["BackSpace"] )
+    #print app.get_actions_for_accel("BackSpace")
+    #print app.get_actions_for_accel("<alt>Up")
+
+
+class BackspaceBack(GObject.GObject, Nautilus.LocationWidgetProvider):
+    def __init__(self):
+        pass
+    
+    def get_widget(self, uri, window):
+        ok()
+        return None
