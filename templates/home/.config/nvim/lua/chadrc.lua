0cd1a6ea3703ac982329ef53065d05f2bbd4ee70
Yuga <myugan59@gmail.com>
2026-08-23 01:25:34 +0700

    nvim: statusline theme -> vscode_colored

--- templates/home/.config/nvim/lua/chadrc.lua
+++ templates/home/.config/nvim/lua/chadrc.lua
@@ -15,10 +15,10 @@ M.base46 = {
 }
 
 -- M.nvdash = { load_on_startup = true }
--- M.ui = {
---       tabufline = {
---          lazyload = false
---      }
--- }
+M.ui = {
+	statusline = {
+		theme = "vscode_colored",
+	},
+}
 
 return M
